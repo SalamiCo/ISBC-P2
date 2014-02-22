154aa61452d4fbc9b45228b9d4c02ca6c1b3f7ba
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2014-02-22 16:36:57 +0100

    Blocker behaviour added.

--- src/t131417/SalamiTeamManager.java
+++ src/t131417/SalamiTeamManager.java
@@ -1,5 +1,6 @@
 package t131417;
 
+import t131417.behaviours.BlockerBehaviour;
 import t131417.behaviours.DefenseBehaviour;
 import t131417.behaviours.DriverBehaviour;
 import t131417.behaviours.GoalKeeperBehaviour;
@@ -11,7 +12,7 @@ import teams.ucmTeam.TeamManager;
 public final class SalamiTeamManager extends TeamManager {
 
     private Behaviour[] behaviours = { //
-        new NopBehaviour(), new GoalKeeperBehaviour(), new DefenseBehaviour(), new DriverBehaviour() };
+        new NopBehaviour(), new GoalKeeperBehaviour(), new DefenseBehaviour(), new DriverBehaviour(), new BlockerBehaviour() };
 
     @Override
     public Behaviour[] createBehaviours () {
@@ -29,6 +30,9 @@ public final class SalamiTeamManager extends TeamManager {
 
             case 3:
                 return behaviours[3];
+            
+            case 4:
+            	return behaviours[4];
 
             default:
                 return behaviours[0];
---
+++ src/t131417/behaviours/BlockerBehaviour.java
@@ -0,0 +1,95 @@
+package t131417.behaviours;
+
+import t131417.RobotUtils;
+import teams.rolebased.WorldAPI;
+import teams.ucmTeam.Behaviour;
+import teams.ucmTeam.RobotAPI;
+import EDU.gatech.cc.is.util.Vec2;
+
+public class BlockerBehaviour extends Behaviour {
+	
+	/**
+     * State used internally by this behaviour.
+     * 
+     * @author Daniel Escoz
+     * @author Pedro Morgado
+     * @author Arturo Pareja
+     */
+	
+    private static enum State {    	
+        /** Go to the opponent's goal */
+        GOTO,
+
+        /** Block the closest opponent */
+        BLOCK
+    }
+
+    private RobotAPI robot;
+
+    private State state;
+
+	@Override
+	public void configure() {
+		
+	}
+
+	@Override
+	public void end() {
+		
+	}
+
+	@Override
+	public void onInit(RobotAPI robot) {
+		this.robot = robot;
+        state = State.GOTO;		
+	}
+
+	@Override
+	public void onRelease(RobotAPI robot) {
+		this.robot = null;		
+	}
+
+	@Override
+	public int takeStep() {		
+		switch (state) {
+		
+			case GOTO: {
+				stepGoto();
+				break;
+			}
+		
+	        case BLOCK: {
+	            stepBlock();
+	            break;
+	        }
+     
+		}
+
+	    robot.setDisplayString("BLOCKER | " + state);
+	    return WorldAPI.ROBOT_OK;
+	}
+	
+	private void stepGoto () {
+		Vec2 goal = robot.getOpponentsGoal();
+
+        if (goal.r >= robot.getPlayerRadius() * 5) {
+            RobotUtils.moveEgo(robot, goal);
+
+        } else {
+            state = State.BLOCK;
+        }
+	}
+	
+	private void stepBlock () {
+		Vec2 goal = robot.getOpponentsGoal();
+
+       	robot.blockClosest();
+       	
+       	//Always close to the opponent's area
+       	if (goal.r >= robot.getPlayerRadius() * 10) {
+       		state = State.GOTO;
+        }
+		
+    }
+
+}
