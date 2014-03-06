40ae735b93dff9ff44cda39a4d15c783bd1cbd35
Pedro Morgado <pedro@riderstate.com>
2014-03-06 12:43:20 +0100

    Last changes

--- src/t1314grupo17/ChoppedTeamManager.java
+++ src/t1314grupo17/ChoppedTeamManager.java
@@ -22,7 +22,7 @@ import teams.ucmTeam.TeamManager;
 
 public final class ChoppedTeamManager extends TeamManager {
 
-    private static final File CBRFILE = new File("./robots/t1314grupo17/cbr");
+    private static final File CBRFILE = new File("./robots/t1314grupo17/cases.cbr");
 
     private long lastTick = 0L;
 
